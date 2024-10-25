8e28f99c149c1b59b0689ec3d53e33a4f6f41c50
aster <137767097+aster-void@users.noreply.github.com>
2024-10-25 23:08:22 +0900

    create postgres.sh

---
+++ postgres.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Postgres on Docker
+# doc: https://hub.docker.com/_/postgres
+
+# アプリケーションサーバーを Docker で立てている場合、セキュリティ上の理由でホスト OS の他のポートにアクセスできないため、
+# この Postgres のコンテナはアプリケーションサーバーをホスト OS で実行している場合のみ使える。
+# doc: (昔見つけた記憶はあるが、探しても見つからなかったのでそれっぽい記述のものがあったら貼ってください)
+
+# to use this, change .env:DATABASE_URL to
+# DATABASE_URL=postgres://user:password@localhost:5432/database
+docker run --rm --name postgres -p 5432:5432 \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_USER=user \
+  -e POSTGRES_DB=database \
+  postgres:latest
