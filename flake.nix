216edd3d2feeb0d04074f656a8f24caa6d3c5a20
aster <137767097+aster-void@users.noreply.github.com>
2025-04-30 00:59:21 +0900

    sync flake

--- flake.nix
+++ flake.nix
@@ -1,32 +1,27 @@
 # Nix Flake: 依存関係を統一的に管理し、バ＝ジョンを固定する。
 # Nix 公式はドキュメントなんて丁寧なものを書いてくれないので、適当な Zenn の記事読み漁るか Youtube で Nix 人の動画見るかして勉強してください。
-
-# Init Command: `nix flake init`
 # 必須: flake.lock
 # 推奨: .envrc
 {
-  description = "Templates"; # パッケージの説明。置き換えなくても動くが、多分置き換えたいだろう。
+  description = "Templates"; # パッケージの説明。置き換えなくても動く
 
   inputs = {
-    # <template repeat desc="outputs の引数に入る入力のリスト" example="fenix.url=\"...\"" />
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    prisma-utils.url = "github:VanCoding/nix-prisma-utils";
-    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { nixpkgs, flake-utils, prisma-utils, fenix, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      prisma = import ./prisma { inherit prisma-utils pkgs; };
-      toolchain = import ./rust-app { inherit system fenix; };
-    in
-    {
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
       # パッケージの設定
-      # <template optional />
-      # `nix build` を実行すると /nix/store に pkgs.hello の artifact が生成され、`nix run` を実行すると stdout に `Hello world!` が出力されるはずです。
+      # `nix run github:ut-code/file-templates` などとしてやると動くのが確認できる。
       packages.default = pkgs.hello;
-      # More examples: 
+      # More examples:
       # - simple stdenv.mkDerivation: https://qiita.com/hnakano863/items/4abfa4475ddc4c8684b1
       # - rustPlatform.buildRustPackage: https://zenn.dev/asa1984/books/nix-hands-on/viewer/ch04-02-rust-project
       # - buildGoModule: https://nixos.org/manual/nixpkgs/stable/#sec-language-go
@@ -34,23 +29,15 @@
 
       # 開発シェルの設定
       devShell = pkgs.mkShell {
-        name = "Template Development Shell"; # devShell の名前
-        src = ./.;
-        buildInputs = with pkgs; [
-          # <template repeat desc="開発中に使うパッケージ" example="biome" />
+        packages = with pkgs; [
           bun
-          just
-          biome
           lefthook
-          toolchain
-          cargo-watch
         ];
 
         # 開発環境に入ったときに実行するシェルフック。
         shellHook = ''
-          # <template repeat example="export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib" />
           lefthook install
-        '' + prisma.shellHook;
+        '';
       };
     });
 }
